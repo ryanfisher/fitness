3bec9737ab079499fb3717782159e69d90c98231
Ryan Fisher <ryancfisher@gmail.com>
2013-04-17 22:06:32 -0700

    Add root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Fitness::Application.routes.draw do
+  root :to => 'root#index'
+
   namespace :api do
     namespace :v1 do
       resources :weighings
     end
   end
-
-  root :to => 'ember#start'
 end
---
+++ spec/views/root/index.html.erb_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe "root/index.html.erb" do
+end
