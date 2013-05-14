cb92f2b8a4afa9cac14ca90303943c308c42aca9
Ryan Fisher <ryancfisher@gmail.com>
2013-05-13 22:19:24 -0700

    Add new controller action to push new weighing object

--- app/assets/javascripts/controllers/weighings_controller.js
+++ app/assets/javascripts/controllers/weighings_controller.js
@@ -1,2 +1,12 @@
 Fitness.WeighingsController = Ember.ArrayController.extend({
+  addWeighing: function () {
+    this.pushObject({
+      weight: this.get('newWeight'),
+      body_fat: this.get('newBodyFat'),
+      body_water: this.get('newBodyWater'),
+      muscle_mass: this.get('newMuscleMass'),
+      bone_mass: this.get('newBoneMass'),
+      date: this.get('newDate')
+    });
+  }
 });
