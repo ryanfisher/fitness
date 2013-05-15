85a5524d68566f93228d0846c09b4c1ab9fd5b21
Ryan Fisher <ryancfisher@gmail.com>
2013-05-14 21:20:02 -0700

    Add weighing route that resets controller array

--- app/assets/javascripts/routes/weighings_route.js
+++ app/assets/javascripts/routes/weighings_route.js
@@ -1,5 +1,8 @@
 Fitness.WeighingsRoute = Ember.Route.extend({
   model: function() {
     return Fitness.Weighing.find();
+  },
+  setupController: function(controller) {
+    controller.set('content', []);
   }
 });
