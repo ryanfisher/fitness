d00d8056acf659923a55f9d5247a918645961dd6
Ryan Fisher <ryancfisher@gmail.com>
2013-12-15 21:04:12 -0800

    Add user association to weighing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :username
 
   validates :email, :username, :presence => true, :uniqueness => true
+
+  has_many :weighings
 end
--- app/models/weighing.rb
+++ app/models/weighing.rb
@@ -1,4 +1,6 @@
 class Weighing < ActiveRecord::Base
   attr_accessible :body_fat, :body_water, :bone_mass, :date, :muscle_mass, :weight
   validates_uniqueness_of :date
+
+  belongs_to :user
 end
