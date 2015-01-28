9f05ce672ba642c76502740c1dc4e5ca1a7c4719
Ryan Fisher <ryancfisher@gmail.com>
2015-01-27 22:43:41 -0800

    Upgrade ruby and gems

--- app/models/weighing.rb
+++ app/models/weighing.rb
@@ -1,5 +1,5 @@
 class Weighing < ActiveRecord::Base
-  attr_accessible :body_fat, :body_water, :bone_mass, :date, :muscle_mass, :weight
+  # attr_accessible :body_fat, :body_water, :bone_mass, :date, :muscle_mass, :weight
   validates_uniqueness_of :date
 
   belongs_to :user
