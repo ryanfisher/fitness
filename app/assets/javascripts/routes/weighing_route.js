2d293a6c9d4c839c73eb3e2355c3b2dab9b38262
Ryan Fisher <ryancfisher@gmail.com>
2015-02-05 22:11:28 -0800

    Create inputView for weighing form

--- app/assets/javascripts/routes/weighing_route.js
+++ app/assets/javascripts/routes/weighing_route.js
@@ -2,7 +2,8 @@ Fitness.WeighingRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('weighing');
   },
-  afterModel: function(weighing, transition) {
-    this.transitionTo('weighings');
-  }
+  // @todo figure this stuff out
+  // afterModel: function(weighing, transition) {
+  //   this.transitionTo('weighings');
+  // }
 });
--- app/assets/javascripts/views/weighing_view.js
+++ app/assets/javascripts/views/weighing_view.js
@@ -1,2 +1,4 @@
 Fitness.WeighingView = Ember.View.extend({
+  inputView: Ember.TextField.extend({
+  }),
 });
